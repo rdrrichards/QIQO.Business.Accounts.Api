09ea91a2f94db660797ba5558818cb7b9a147592
R. Richards <rdrrichards@hotmail.com>
2018-12-04 15:53:02 -0600

    Core 2.2 updates, including docker

--- QIQO.Business.Accounts.Api/Startup.cs
+++ QIQO.Business.Accounts.Api/Startup.cs
@@ -55,10 +55,10 @@ namespace QIQO.Business.Accounts.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
 
             app.UseCors("AnyOrigin");
             app.UseMvc();
